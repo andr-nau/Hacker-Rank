bdaf573b719679b7e25aee14b4d74490e43bd0b1
Andrii Naumov <andriy.naumov@gmail.com>
2023-12-10 16:39:01 +0100

    Update String-Formatting.py

--- prepare-python/basics/String-Formatting.py
+++ prepare-python/basics/String-Formatting.py
@@ -1,10 +1,25 @@
+"""
+Problem: https://www.hackerrank.com/challenges/python-string-formatting/
+
+Given an integer, , print the following values for each integer i from 1 to n:
+
+    1. Decimal
+    2. Octal
+    3. Hexadecimal (capitalized)
+    4. Binary
+
+Prints
+The four values must be printed on a single line in the order specified above for each i from 1 to 'number'.
+Each value should be space-padded to match the width of the binary value of 'number' and the values should be separated by a single space.
+"""
+
 def print_formatted(number):
     #Set max width of all columns from width of HEX representation of the max number
     spacing = len('{:b}'.format(number))
     
     for num in range(number):
-        for base in 'doXb':
+        for base in 'doXb': #Loop over the base for format: d - decimal, o - actal, X - hexadecimal, b - binary
             print('{0:{width}{base}}'.format(num+1, width=spacing, base=base), end=' ')
         print()
 
-print_formatted(17)
+print_formatted(17) # HR test case
